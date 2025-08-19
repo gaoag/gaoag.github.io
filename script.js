fbe5d9fd3ccc49d9acf4b188dc227f35d657ff58
Alex Gao <alex.keming.gao@gmail.com>
2025-08-19 10:06:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 window.onload = function(){
-    setInterval(switchNames, 17776)
+    setInterval(switchNames, 8000)
 };
 
 function switchNames() {
@@ -14,6 +14,7 @@ function switchNames() {
 
 
 }
+
 
 
 
